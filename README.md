cbfd12cbdc97c460ace321a14c8082a56ac0ce79
Ryan Liptak <squeek502@hotmail.com>
2021-10-03 17:42:21 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+zigescape
+=========
+
+A tool for converting between binary data and [Zig](https://ziglang.org/) string literals.
+
+Basic example (more can be found below):
+
+```sh
+$ echo '00 01 02 03 04' | xxd -r -p | zigescape
+"\x00\x01\x02\x03\x04"
+```
+
+## Building / Installation
+
+### From Source
+
+Requires latest master of Zig.
+
+1. Clone this repository and its submodules (`git clone --recursive` to get submodules)
+2. `zig build`
+3. The compiled binary will be in `zig-out/bin/zigescape`
+4. `mv` or `ln` the binary somewhere in your `PATH`
+
+## Usage
+
+```
+Usage: zigescape [-hs] [-o <PATH>] <INPUT>
+
+<INPUT>: Either a path to a file or a Zig string literal (if using --string)
+         If <INPUT> is not specified, then stdin is used.
+
+Available options:
+	-h, --help         	Display this help and exit.
+	-o, --output <PATH>	Output file path (stdout is used if not specified).
+	-s, --string       	Specifies that the input is a Zig string literal.
+	                   	Output will be the parsed string.
+```
+
+## Examples
+
+### Converting *to* a string literal:
+
+```
+zigescape path/to/file
+```
+
+or
+
+```
+zigescape < path/to/file
+```
+
+or, if you want to output to a file:
+
+```
+zigescape path/to/file -o path/to/outfile
+```
+
+### Converting *from* string literal
+
+> Note: shell escaping of arguments can mess with the string literal before it gets parsed, so it's best to use single quotes to bypass shell escaping. 
+
+```
+zigescape --string '"hello world\n"'
+```
+
+The double quotes are optional, `zigescape` will add them if they are missing:
+
+```
+zigescape --string 'hello world\n'
+```
+
+### Some silly examples
+
+`cat`ing a file by converting it to a string literal and then parsing it to stdout:
+
+```
+zigescape path/to/file | zigescape --string
+```
+
+Copying a file by converting it to a string literal and then parsing it:
+
+```
+zigescape path/to/file.orig | zigescape --string -o path/to/file.copy
+```
---
+++ src/main.zig
@@ -0,0 +1,92 @@
+const std = @import("std");
+const clap = @import("clap");
+
+pub fn main() anyerror!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == false);
+    const allocator = &gpa.allocator;
+
+    const params = comptime [_]clap.Param(clap.Help){
+        clap.parseParam("-h, --help            Display this help and exit.") catch unreachable,
+        clap.parseParam("-o, --output <PATH>   Output file path (stdout is used if not specified).") catch unreachable,
+        clap.parseParam("-s, --string          Specifies that the input is a Zig string literal.\nOutput will be the parsed string.") catch unreachable,
+        clap.parseParam("<INPUT>               ") catch unreachable,
+    };
+
+    var diag = clap.Diagnostic{};
+    var args = clap.parse(clap.Help, &params, .{ .diagnostic = &diag, .allocator = allocator }) catch |err| {
+        diag.report(std.io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer args.deinit();
+
+    if (args.flag("--help")) {
+        const writer = std.io.getStdErr().writer();
+        //try writer.writeAll("Usage: grindcov [options] -- <cmd> [<args>...]\n\n");
+        //try writer.writeAll("Available options:\n");
+        try writer.writeAll("Usage: zigescape ");
+        try clap.usage(writer, &params);
+        try writer.writeAll("\n\n");
+        try writer.writeAll(
+            \\<INPUT>: Either a path to a file or a Zig string literal (if using --string)
+            \\         If <INPUT> is not specified, then stdin is used.
+        );
+        try writer.writeAll("\n\n");
+        try writer.writeAll("Available options:\n");
+        try clap.help(writer, &params);
+        return;
+    }
+
+    const outfile = outfile: {
+        if (args.option("--output")) |output_path| {
+            break :outfile try std.fs.cwd().createFile(output_path, .{});
+        } else {
+            break :outfile std.io.getStdOut();
+        }
+    };
+    const writer = outfile.writer();
+
+    var data_allocated = false;
+    const data = data: {
+        if (args.flag("--string") and args.positionals().len > 0) {
+            break :data args.positionals()[0];
+        }
+        const infile = infile: {
+            if (args.positionals().len > 0) {
+                const path = args.positionals()[0];
+                break :infile try std.fs.cwd().openFile(path, .{});
+            } else {
+                break :infile std.io.getStdIn();
+            }
+        };
+        data_allocated = true;
+        break :data try infile.readToEndAlloc(allocator, std.math.maxInt(usize));
+    };
+    defer if (data_allocated) allocator.free(data);
+
+    if (args.flag("--string")) {
+        var line = data;
+        if (std.mem.indexOfAny(u8, line, "\r\n")) |line_end| {
+            line = line[0..line_end];
+        }
+        var line_allocated = false;
+        // wrap in quotes if it's not already
+        if (line.len >= 2 and (line[0] != '"' or line[line.len - 1] != '"')) {
+            var buf = try allocator.alloc(u8, line.len + 2);
+            buf[0] = '"';
+            std.mem.copy(u8, buf[1..], line);
+            buf[buf.len - 1] = '"';
+
+            line = buf;
+            line_allocated = true;
+        }
+        defer if (line_allocated) allocator.free(line);
+
+        const parsed = try std.zig.string_literal.parseAlloc(allocator, line);
+        defer allocator.free(parsed);
+
+        try writer.writeAll(parsed);
+    } else {
+        try writer.print("\"{}\"\n", .{std.zig.fmtEscapes(data)});
+    }
+}
